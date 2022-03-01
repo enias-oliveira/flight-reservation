6c403ec50a3c25ecf0c3155758608c62faa46b83
Enias Oliveira <eniasoliveira27@gmail.com>
2022-03-01 01:36:02 -0300

    feat: adds report generation and filter by date

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -5,6 +5,17 @@ defmodule Flightex.Bookings.Report do
     File.write(filename, build_bookings_list())
   end
 
+  def generate_report_by_date(from_date, to_date) do
+    bookings =
+      Agent.get_all()
+      |> Enum.filter(fn {_order_id, %{complete_date: complete_date}} ->
+        (from_date <= complete_date and complete_date <= to_date)
+      end)
+      |> Enum.map(fn {_order_id, order} -> parse_to_csv_line(order) end)
+
+    File.write("report.csv", bookings)
+  end
+
   defp build_bookings_list(),
     do: Agent.get_all() |> Enum.map(fn {_order_id, order} -> parse_to_csv_line(order) end)
 
@@ -14,6 +25,6 @@ defmodule Flightex.Bookings.Report do
          local_destination: local_destination,
          complete_date: complete_date
        }) do
-    "#{user_id},#{local_origin},#{local_destination},#{complete_date}"
+    "#{user_id},#{local_origin},#{local_destination},#{complete_date}\n"
   end
 end
